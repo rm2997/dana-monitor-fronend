896ac8a5c7b1de78be43b52a2182d86696d6f679
rm2997 <rm2997@gmail.com>
2024-08-26 16:17:31 +0330

    mend

--- src/lib/loadApiData.js
+++ src/lib/loadApiData.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 
 export async function loadApiData(){        
-  try {
-    const {data}=  await axios.get(`${process.env.REACT_APP_API_URL}/GetDanaStatus`)
+  try {  
+    const apiAddress=`${process.env.REACT_APP_API_URL}/GetDanaStatus`;
+    console.log(`address is : ${apiAddress}`);    
+    const {data}=  await axios.get(apiAddress)
     .then(result=>result);
     return data;    
   } catch (error) {
