da84514f89f98ead110774632f59bc397ac8361d
rm2997 <rm2997@gmail.com>
2024-09-07 14:19:02 +0330

    mend

--- src/lib/loadApiData.js
+++ src/lib/loadApiData.js
@@ -1,18 +1,19 @@
-import { type } from "@testing-library/user-event/dist/type";
 import axios from "axios";
 
 export async function loadApiData(apiAddress) {
   try {
     if (!apiAddress) return;
 
+    // const fullAddress = `http://dana-monitor-backend:4000/api/GetDanaStatus`;
+    console.log(`Loaded from config.json : ${apiAddress}`);
     const fullAddress = `${apiAddress}/GetDanaStatus`;
     console.log(`Waiting for API: ${fullAddress}`);
-
-    const { data } = await axios({
-      method: "get",
-      url: fullAddress,
-      headers: { "Access-Control-Allow-Origin": "*" },
-    }).then((result) => result);
+    const { data } = await axios
+      .get(fullAddress)
+      .then((result) => result)
+      .catch((err) => {
+        console.log(err);
+      });
     return data;
   } catch (error) {
     console.log("Failed to load data from API: ", error.message);
