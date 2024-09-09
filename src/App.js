3051403888ba8b8138762a0e5bc7aab336ce56b3
rm2997 <rm2997@gmail.com>
2024-09-09 17:10:10 +0330

    Fix getting data

--- src/App.js
+++ src/App.js
@@ -12,11 +12,23 @@ import ResponseTimeChart from "./components/chart/ResponseTimeChart";
 
 let timer = null;
 function App() {
-  const [danaStatus, setDanaStatus] = useState(null);
+  const [danaStatus, setDanaStatus] = useState({
+    GateWayName: "",
+    BankName: "",
+    HostIP: "",
+    ThreadCount: 0,
+    DiskSpace: 0,
+    CpuUsage: 0,
+    MemoryUsage: 0,
+  });
   const [portStatus, setPortStatus] = useState(false);
   const [pingStatus, setPingStatus] = useState(false);
   const [gateStatus, setGateStatus] = useState(false);
-  const [luStatus, setLuStatus] = useState(null);
+  const [luStatus, setLuStatus] = useState({
+    AllLUCount: 0,
+    FailedLUCount: 0,
+    AvailableLUCount: 0,
+  });
   const [responseTimes, setResponseTimes] = useState(null);
   const [myChartData, setMyChartData] = useState(null);
   const [playing, setPlaying] = useState(false);
@@ -25,6 +37,14 @@ function App() {
   useEffect(() => {
     loadConfig();
   }, []);
+
+  useEffect(() => {
+    console.log("from use Effect for dana status", danaStatus);
+  }, [danaStatus]);
+
+  useEffect(() => {
+    console.log("from use Effect for lu status", luStatus);
+  }, [luStatus]);
 
   useEffect(() => {
     loadApiData();
@@ -69,7 +89,7 @@ function App() {
       }
       const result = await response.json();
 
-      setMyChartData(result);
+      //setMyChartData(result);
       parseReceivedData(result);
     } catch (err) {
       console.log(`Failed to load API data: ${err}`);
@@ -77,14 +97,49 @@ function App() {
   };
 
   function parseReceivedData(data) {
-    data.forEach((element) => {
-      if (element) {
-        const { PingStatus, GateStatus, PortStatus } = JSON.parse(element).Data;
-        if (PingStatus) setPingStatus(PingStatus);
-        if (GateStatus) setGateStatus(GateStatus);
-        if (PortStatus) setPortStatus(PortStatus);
-      }
-    });
+    try {
+      data.forEach((element) => {
+        if (element) {
+          const {
+            PingStatus,
+            GateStatus,
+            PortStatus,
+            AllLUCount,
+            FailedLUCount,
+            AvailableLUCount,
+            GateWayName,
+            BankName,
+            HostIP,
+            ThreadCount,
+            DiskSpace,
+            CpuUsage,
+            MemoryUsage,
+          } = JSON.parse(element).Data;
+          const tmpLuStatus = { ...luStatus };
+          const tmpDanaStatus = { ...danaStatus };
+
+          if (PingStatus) setPingStatus(PingStatus);
+          if (GateStatus) setGateStatus(GateStatus);
+          if (PortStatus) setPortStatus(PortStatus);
+
+          if (AllLUCount) tmpLuStatus.AllLUCount = AllLUCount;
+          if (FailedLUCount) tmpLuStatus.FailedLUCount = FailedLUCount;
+          if (AvailableLUCount) tmpLuStatus.AvailableLUCount = AvailableLUCount;
+          setLuStatus({ ...tmpLuStatus });
+
+          if (GateWayName) tmpDanaStatus.GateWayName = GateWayName;
+          if (BankName) tmpDanaStatus.BankName = BankName;
+          if (HostIP) tmpDanaStatus.HostIP = HostIP;
+          if (ThreadCount) tmpDanaStatus.ThreadCount = ThreadCount;
+          if (DiskSpace) tmpDanaStatus.DiskSpace = DiskSpace;
+          if (CpuUsage) tmpDanaStatus.CpuUsage = CpuUsage;
+          if (MemoryUsage) tmpDanaStatus.MemoryUsage = MemoryUsage;
+          setDanaStatus({ ...tmpDanaStatus });
+        }
+      });
+    } catch (err) {
+      console.log(`Error in message parser: ${err}`);
+    }
   }
 
   function handleRefresh() {
@@ -98,7 +153,7 @@ function App() {
     <MainArea>
       <DrawerMenu
         DanaName={
-          danaStatus
+          danaStatus.BankName + danaStatus.GateWayName !== ""
             ? danaStatus.BankName + danaStatus.GateWayName
             : "Disconnected"
         }
@@ -106,7 +161,7 @@ function App() {
         <div style={{ margin: "5px", display: "flex", flexDirection: "row" }}>
           <div style={{ width: "70%", margin: "5px" }}>
             <TransactionChart
-              dataSet={myChartData}
+              dataSet={!responseTimes ? responseTimes : []}
               handelFetch={(e) => handleRefresh()}
               handlePlaying={() => handelPlaying(!playing)}
               isPlaying={playing}
@@ -114,7 +169,7 @@ function App() {
             />
           </div>
           <div style={{ width: "30%", margin: "5px" }}>
-            <ResponseTimeChart dataSet={responseTimes ? responseTimes : []} />
+            <ResponseTimeChart dataSet={!responseTimes ? responseTimes : []} />
           </div>
         </div>
 
@@ -134,29 +189,27 @@ function App() {
             <StatusElement label={"Host Ping"} value={pingStatus} />
             <StatusElement label={"Host Telnet"} value={portStatus} />
             <StatusElement
-              label={`Thread Count: ${danaStatus ? danaStatus.ThreadCount : 0}`}
-              value={
-                danaStatus ? (danaStatus.ThreadCount ? true : false) : false
-              }
+              label={`Thread Count: ${danaStatus.ThreadCount}`}
+              value={danaStatus.ThreadCount}
             />
           </Card>
           <Card style={{ width: "40%", margin: "10px" }}>
             <MyProgressChart
-              value={danaStatus ? danaStatus.CpuUsage : 100}
+              value={danaStatus.CpuUsage}
               labelName="Cpu Usage"
             />
             <MyProgressChart
-              value={danaStatus ? danaStatus.MemoryUsage : 100}
+              value={danaStatus.MemoryUsage}
               labelName="Memory Usage"
             />
             <MyProgressChart
-              value={danaStatus ? danaStatus.DiskSpace : 100}
+              value={danaStatus.DiskSpace}
               labelName="Disk Usage"
             />
           </Card>
         </div>
       </DrawerMenu>
-      {!myChartData ? <FirstLoading /> : <></>}
+      {myChartData ? <></> : <FirstLoading />}
     </MainArea>
   );
 }
