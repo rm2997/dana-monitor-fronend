635063e75ea4bb112f043784119317099184e356
rm2997 <rm2997@gmail.com>
2024-11-12 15:04:15 +0330

    After redis test , and Post requests are seperated

--- src/configs/ApiMaps.js
+++ src/configs/ApiMaps.js
@@ -10,18 +10,18 @@ export class ApiMaps {
 
   static LoginRequest = new ApiMaps("/auth/login", false, "POST");
   static LuStatus = new ApiMaps("/GetLuStatus", true);
-  static AddLuPack = new ApiMaps("/GetLuPack", true);
+  static AddLuPack = new ApiMaps("/AddLuPack", true, "POST");
   static ResponseTimes = new ApiMaps("/GetResponseTimes", true);
   static HostTransaction = new ApiMaps("/GetHostTransactions", true);
   static PingStatus = new ApiMaps("/GetPingStatus", true);
   static PortStatus = new ApiMaps("/GetPortStatus", true);
   static DanaStatus = new ApiMaps("/GetDanaStatus", true);
   static GateStatus = new ApiMaps("/GetGateStatus", true);
-  static BackupDb = new ApiMaps("/BackupDb", true);
-  static RotateTable = new ApiMaps("/RotateTable", true);
-  static BackupLogFile = new ApiMaps("/BackupLogFile", true);
-  static ReconnectLu = new ApiMaps("/ReconnectLu", true);
-  static OpenGate = new ApiMaps("/OpenGate", true);
-  static CloseGate = new ApiMaps("/CloseGate", true);
+  static BackupDb = new ApiMaps("/BackupDb", true, "POST");
+  static RotateTable = new ApiMaps("/RotateTable", true, "POST");
+  static BackupLogFile = new ApiMaps("/BackupLogFile", true, "POST");
+  static ReconnectLu = new ApiMaps("/ReconnectLu", true, "POST");
+  static OpenGate = new ApiMaps("/OpenGate", true, "POST");
+  static CloseGate = new ApiMaps("/CloseGate", true, "POST");
   static ServicesStatus = new ApiMaps("", true);
 }
